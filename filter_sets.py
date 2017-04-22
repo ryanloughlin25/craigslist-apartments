1c59076748d24188836dfc2d8b9d48e617ac34b2
Sean Grady <seangrady@Seans-MacBook-Pro.local>
2017-04-22 16:50:30 -0700

    added filter sets in a separate file and refactored craigslist_test to use them

---
+++ filter_sets.py
@@ -0,0 +1,16 @@
+filter_sets = {
+    'Inner Richmond': {
+        'max_price': 3500,
+        'min_price': 2000,
+        'bedrooms': 2,
+        'zip_code': 94118,
+        'no_smoking': True,
+    },
+    'Marina': {
+        'max_price': 5000,
+        'min_price': 2000,
+        'bedrooms': 2,
+        'zip_code': 94123,
+        'no_smoking': True,
+    },
+}
